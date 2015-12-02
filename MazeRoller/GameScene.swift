2b7bfb40f2326f53f4e818676844b357224503c0
Eren Buyru <erenbuyru@gmail.com>
2015-12-02 16:23:17 +0000

    It needs fix doesnt work

--- MazeRoller/GameScene.swift
+++ MazeRoller/GameScene.swift
@@ -8,38 +8,52 @@
 
 import SpriteKit
 
+enum CollisionTypes: UInt32 {
+    case Hero = 1
+    case Wall = 2
+    case Vortex = 8
+    case Finish = 16
+    //add vortex token enum
+}
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+       
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func constructScene() {
+        if let scenePath = NSBundle.mainBundle().pathForResource("level1", ofType: "txt"){
+            if let levelString = try? NSString(contentsOfFile: scenePath, encoding: NSUTF8StringEncoding){
+                let lines = levelString.componentsSeparatedByString("\n") as [String]
+                
+                for (row,line) in lines.reverse().enumerate() {
+                    let line2 = line.characters
+                    for (column,letter) in line2.enumerate() {
+                        let position = CGPoint(x: (32 * column) + 16 ,y: (32 * row) + 16)
+                        
+                        if letter == "x" {
+                            //load wall
+                            let node = SKSpriteNode(imageNamed: "wall")
+                            node.position = position
+                            
+                            node.physicsBody = SKPhysicsBody(rectangleOfSize: node.size)
+                            node.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
+                            node.physicsBody?.dynamic = false
+                            
+                            addChild(node)
+                            
+                        }else if letter == "v"{
+                            //load vortex
+                        }//end of if letter v
+                    }//end of letter load
+                }
+            }
+        }//end of if scenePath
+    }
+    
 }
